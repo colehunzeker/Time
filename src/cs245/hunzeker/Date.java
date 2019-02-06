e30617e42028ba3c46bbd4b5cfb55abd1e04579e
colehunzeker <cehunzeker@coe.edu>
2019-02-05 20:10:33 -0600

    Initial Commit

---
+++ src/cs245/hunzeker/Date.java
@@ -0,0 +1,71 @@
+package cs245.hunzeker;
+import java.util.Scanner;
+
+public class Date {
+    private int day;
+    private int month;
+    private int year;
+    Scanner goodDate = new Scanner(System.in);
+    public int daysInMonth;
+
+
+
+    public Date(int day, int month, int year) {
+        System.out.println("How many days are in this month?");
+        daysInMonth = goodDate.nextInt();
+        try {
+            setDay(day);
+        }
+        catch (IllegalArgumentException e) {
+            this.day = day % daysInMonth;
+            month += day / daysInMonth;
+        }
+        try {
+            setMonth(month);
+        }
+        catch(IllegalArgumentException e){
+            this.month = month % 12;
+            year += month / 12;
+        }
+        try{
+            setYear(year);
+        }
+        catch (IllegalArgumentException e){
+            this.year = -year;
+        }
+
+    }
+    public String toString(){
+        return (day + "/" + month + "/" + year);
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        if (day > daysInMonth || day < 0)
+            throw new IllegalArgumentException("Invalid day");
+        this.day = day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        if (month < 0 || month > 12)
+            throw new IllegalArgumentException("Invalid Month");
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        if (year < 0)
+            throw new IllegalArgumentException("Invalid Year");
+        this.year = year;
+    }
+}
