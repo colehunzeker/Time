7acc4070797fe0aecb066d7058ddd1b1bd9b1b76
colehunzeker <cehunzeker@coe.edu>
2019-02-05 18:58:56 -0600

    Initial Commit

---
+++ src/cs245/hunzeker/Main.java
@@ -0,0 +1,20 @@
+package cs245.hunzeker;
+
+public class Main {
+
+    public static void main(String[] args) {
+      /*  Time t = new Time(10, 8, 45);
+        System.out.println(t);
+        t.setSeconds(41);
+        System.out.println(t);
+        try {
+            t.setSeconds(983);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Oops");
+        }
+        System.out.println(t);
+        */
+        Time t2 = new Time(24, 0, 1);
+        System.out.println(t2);
+    }
+}
---
+++ src/cs245/hunzeker/Time.java
@@ -0,0 +1,66 @@
+package cs245.hunzeker;
+
+public class Time {
+    private int hours;
+    private int minutes;
+    private int seconds;
+
+
+    public Time(int hours, int minutes, int seconds) {
+        try{
+            setSeconds(seconds);
+        }
+        catch(IllegalArgumentException e){
+            this.seconds = seconds % 60;
+            minutes += seconds / 60;
+        }
+        try {
+            setMinutes(minutes);
+        }
+        catch(IllegalArgumentException e){
+            this.minutes = minutes % 60;
+            hours += minutes / 60;
+        }
+        try {
+            setHours(hours);
+        }
+        catch (IllegalArgumentException e){
+            this.hours = hours % 24;
+
+        }
+    }
+    public String toString(){
+        return hours + ":" + minutes + ":" + seconds;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public void setHours(int hours) {
+        if (hours < 0 || hours > 23)
+            throw new IllegalArgumentException("Invalid Hours");
+        this.hours = hours;
+    }
+
+    public void setMinutes(int minutes) {
+        if (minutes <0 || minutes > 59)
+            throw new IllegalArgumentException("Invalid Minutes");
+        this.minutes = minutes;
+    }
+
+    public void setSeconds(int seconds) {
+        if (seconds < 0 || seconds > 59){
+            throw new IllegalArgumentException("Invalid Seconds");
+        }
+        this.seconds = seconds;
+    }
+}
